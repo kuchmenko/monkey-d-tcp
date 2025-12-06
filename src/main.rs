f23c03335b38ba31eb9d95195bfb6045db0ce6ab
Ivan Kuchmenko <ivan@kuchmenko.com>
2025-12-06 18:26:10 +0200

    graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,112 @@
+use std::time::Duration;
+
 use tokio::{
     io::{AsyncReadExt, AsyncWriteExt},
     net::{TcpListener, TcpStream},
+    select,
+    time::sleep,
 };
+use tokio_util::sync::CancellationToken;
 
 #[derive(Debug, thiserror::Error)]
 enum AppError {
     #[error("Socket error: {0}")]
-    SocketError(std::io::Error),
+    SocketError(#[from] std::io::Error),
+}
+
+async fn run_server(
+    output_ip: &str,
+    src_ip: &str,
+    graceful_token: &CancellationToken,
+) -> Result<(), AppError> {
+    let input_listener = TcpListener::bind(src_ip).await?;
+
+    loop {
+        if graceful_token.is_cancelled() {
+            println!("Stopped accepting connections");
+            return Ok(());
+        }
+
+        let (mut stream_a, _) = input_listener.accept().await?;
+        let stream_b = TcpStream::connect(output_ip).await?;
+
+        let (mut a_read, mut a_write) = stream_a.into_split();
+        let (mut b_read, mut b_write) = stream_b.into_split();
+        let token = CancellationToken::new();
+        let token_clone = token.clone();
+
+        let read_graceful_token = graceful_token.clone();
+        let write_graceful_token = graceful_token.clone();
+
+        tokio::spawn(async move {
+            loop {
+                let mut buf = [0u8; 1024];
+                select! {
+                    result = a_read.read(&mut buf) => {
+                        if let Ok(n) = result {
+                            if n == 0 {
+                                token.cancel();
+                                break;
+                            }
+                            b_write.write_all(&buf[..n]).await.unwrap();
+                        }
+                    },
+                    _ = read_graceful_token.cancelled() => {
+                            println!("Gracefully stopping read for client {}", a_read.peer_addr().unwrap());
+                            break;
+                        }
+                    _ = token.cancelled() => {
+                        println!("Write stream is closed for client {}", a_read.peer_addr().unwrap());
+                        break;
+                    }
+                };
+            }
+        });
+
+        tokio::spawn(async move {
+            loop {
+                let mut buf = [0u8; 1024];
+                select! {
+                    result = b_read.read(&mut buf) => {
+                        if let Ok(n) = result {
+                            if n == 0 {
+                                token_clone.cancel();
+                                break;
+                            }
+                            a_write.write_all(&buf[..n]).await.unwrap();
+                        }
+                    },
+                    _ = write_graceful_token.cancelled() => {
+                        println!("Gracefully stopping write for dest client {}", b_read.peer_addr().unwrap());
+                        break;
+                    }
+                    _ = token_clone.cancelled() => {
+                        println!("Read stream is closed for dest client {}", b_read.peer_addr().unwrap());
+                        break;
+                    }
+                };
+            }
+        });
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let output_ip = "127.0.0.1:8080";
     let src_ip = "127.0.0.1:8081";
+    let graceful_token = CancellationToken::new();
 
-    let input_listener = TcpListener::bind(src_ip).await?;
+    select! {
+        _ = run_server(output_ip, src_ip, &graceful_token) => {},
+        _ = tokio::signal::ctrl_c() => {
+            graceful_token.cancel();
+            let graceful_period = Duration::from_secs(1);
 
-    loop {
-        let (mut stream_a, _) = input_listener.accept().await?;
-        let stream_b = TcpStream::connect(output_ip).await?;
+            sleep(graceful_period).await;
+            println!("Exiting...");
+            std::process::exit(0);
+        },
+    };
 
-        let (mut a_read, mut a_write) = stream_a.into_split();
-        let (mut b_read, mut b_write) = stream_b.into_split();
-
-        tokio::spawn(async move {
-            loop {
-                let mut buf = [0; 1024];
-                let n = a_read.read(&mut buf).await.unwrap();
-                if n == 0 {
-                    break;
-                }
-                b_write.write_all(&buf[..n]).await.unwrap();
-            }
-        });
-
-        tokio::spawn(async move {
-            loop {
-                let mut buf = [0; 1024];
-                let n = b_read.read(&mut buf).await.unwrap();
-                if n == 0 {
-                    break;
-                }
-                a_write.write_all(&buf[..n]).await.unwrap();
-            }
-        });
-    }
+    Ok(())
 }
